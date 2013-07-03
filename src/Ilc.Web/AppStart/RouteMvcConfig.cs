2fc7ed76911de10349c32e165978ccb52fe32f7d
maximilian <maximilian1991@gmail.com>
2013-07-03 12:57:06 +0300

    Added Mvc 4 to the project.

---
+++ src/Ilc.Web/AppStart/RouteMvcConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Ilc.Web.AppStart
+{
+    public class RouteMvcConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Transfers", action = "Sample" }
+            );
+        }
+    }
+}
--- src/Ilc.Web/Global.asax.cs
+++ src/Ilc.Web/Global.asax.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using Ilc.Web.AppStart;
@@ -14,6 +16,9 @@ namespace Ilc.Web
         protected void Application_Start(object sender, EventArgs e)
         {
             new AppHost().Init();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteMvcConfig.RegisterRoutes(RouteTable.Routes);
         }
 
         protected void Session_Start(object sender, EventArgs e)
