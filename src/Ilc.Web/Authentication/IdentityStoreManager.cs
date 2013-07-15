fc70341188672386f41c6c44868450d2407feda1
maximilian <maximilian1991@gmail.com>
2013-07-15 21:02:22 +0300

    Wired validation to persistance store.

--- src/Ilc.Web/Authentication/IdentityStoreManager.cs
+++ src/Ilc.Web/Authentication/IdentityStoreManager.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.Web.Security;
+using Ilc.Core.Contracts;
 using Ilc.Web.Authentication.Contracts;
+using WebMatrix.WebData;
 
 namespace Ilc.Web.Authentication
 {
     public class IdentityStoreManager : IIdentityStoreManager
     {
+        // this is set via DI
+        public IUsersService UsersService { get; set; }
+
         public bool ValidateLocalLogin(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = UsersService.GetByUsername(username);
+            if (user == null) return false;
+
+            if (CheckPassword(username, password)) return false;
+
+            return false;
+        }
+
+
+        private bool CheckPassword(string username, string password)
+        {
+            return Membership.ValidateUser(username, password);
         }
     }
 }
