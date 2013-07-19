73d2a56c0567c8cd605693e623752cc31f4d060a
maximilian <maximilian1991@gmail.com>
2013-07-19 17:58:50 +0300

    Added the interview window for the Interview status.

--- src/Ilc.Web/Scripts/app/tasks/training/Interview.js
+++ src/Ilc.Web/Scripts/app/tasks/training/Interview.js
@@ -25,104 +25,8 @@
         });
 
 
-        var studentsGrid = Ext.create('Ext.grid.Panel', {
+        var studentsGrid = Ext.create('Ilc.tasks.training.grid.Students', {
             store: studentsStore,
-            columns: [
-                {
-                    dataIndex: 'name',
-                    text: 'Student name'
-                },
-                {
-                    xtype: 'actioncolumn',
-                    items: [
-                        {
-                            getClass: function (v, meta, record) {
-                                return record.get('interviewId') == 0 ? 'add-col' : 'view-col';
-                            },
-                            getTip: function (v, meta, record) {
-                                return record.get('interviewId') == 0 ? 'Add interview' : 'View interview';
-                            },
-                            handler: function (grid, rowIndex, colIndex, item, e, record) {
-                                var action = (record.get('interviewId') == 0 ? "Add" : "View");
-
-                                var store = Ext.create('Ext.data.Store', {
-                                    fields: ['text', 'order'],
-                                    data: {
-                                        data: [
-                                            { text: 'A1', order: 1 },
-                                            { text: 'A2', order: 2 },
-                                            { text: 'B1', order: 3 },
-                                            { text: 'B2', order: 4 },
-                                            { text: 'C1', order: 5 },
-                                            { text: 'C2', order: 6 }
-                                        ]
-                                    },
-                                    proxy: {
-                                        type: 'memory',
-                                        reader: {
-                                            type: 'json',
-                                            root: 'data'
-                                        }
-                                    }
-                                });
-                                
-                                var trainersStore = Ext.create('Ext.data.Store', {
-                                    fields: ['name'],
-                                    data: {
-                                        data: [
-                                            { id: 1, name: 'Trainer 1' },
-                                            { id: 2, name: 'Trainer Z' },
-                                            { id: 3, name: 'Professor X' }
-                                        ]
-                                    },
-                                    proxy: {
-                                        type: 'memory',
-                                        reader: {
-                                            type: 'json',
-                                            root: 'data'
-                                        }
-                                    }
-                                });
-
-                                var window = Ext.create('Ext.window.Window', {
-                                    modal: true,
-                                    items: [
-                                        {
-                                            xtype: 'combobox',
-                                            store: store,
-                                            fieldLabel: 'Level',
-                                            queryMode: 'local',
-                                            displayField: 'text',
-                                            valueField: 'order'
-                                        },
-                                        {
-                                            xtype: 'combobox',
-                                            store: store,
-                                            fieldLabel: 'Target level',
-                                            queryMode: 'local',
-                                            displayField: 'text',
-                                            valueField: 'order'
-                                        },
-                                        {
-                                            xtype: 'combobox',
-                                            store: trainersStore,
-                                            fieldLabel: 'Trainer',
-                                            queryMode: 'local',
-                                            displayField: 'name',
-                                            valueField: 'order'
-                                        },
-                                        {
-                                            xtype: 'button',
-                                            text: 'Add'
-                                        }
-                                    ]
-                                });
-                                window.show();
-                            }
-                        }
-                    ]
-                }
-            ]
         });
 
         me.items = [
@@ -132,6 +36,15 @@
                 text: 'Done'
             }
         ];
+
+        studentsGrid.on('addInterview', function(sender, data) {
+            console.log('AddInterview event.');
+        });
+
+        me.addEvents(
+            'addInterview',
+            'allInterviewsAdded'
+        );
 
         me.callParent(arguments);
     }
---
+++ src/Ilc.Web/Scripts/app/tasks/training/grid/Students.js
@@ -0,0 +1,115 @@
+﻿Ext.define('Ilc.tasks.training.grid.Students', {
+    extend: 'Ext.grid.Panel',
+
+
+    constructor: function () {
+        var me = this;
+
+        me.columns = [
+            {
+                dataIndex: 'name',
+                text: 'Student name'
+            },
+            {
+                xtype: 'actioncolumn',
+                items: [
+                    {
+                        getClass: function(v, meta, record) {
+                            return record.get('interviewId') == 0 ? 'add-col' : 'view-col';
+                        },
+                        getTip: function(v, meta, record) {
+                            return record.get('interviewId') == 0 ? 'Add interview' : 'View interview';
+                        },
+                        handler: function(grid, rowIndex, colIndex, item, e, record) {
+                            var action = (record.get('interviewId') == 0 ? "Add" : "View");
+
+                            var store = Ext.create('Ext.data.Store', {
+                                fields: ['text', 'order'],
+                                data: {
+                                    data: [
+                                        { text: 'A1', order: 1 },
+                                        { text: 'A2', order: 2 },
+                                        { text: 'B1', order: 3 },
+                                        { text: 'B2', order: 4 },
+                                        { text: 'C1', order: 5 },
+                                        { text: 'C2', order: 6 }
+                                    ]
+                                },
+                                proxy: {
+                                    type: 'memory',
+                                    reader: {
+                                        type: 'json',
+                                        root: 'data'
+                                    }
+                                }
+                            });
+
+                            var trainersStore = Ext.create('Ext.data.Store', {
+                                fields: ['id', 'name'],
+                                data: {
+                                    data: [
+                                        { id: 1, name: 'Trainer 1' },
+                                        { id: 2, name: 'Trainer Z' },
+                                        { id: 3, name: 'Professor X' }
+                                    ]
+                                },
+                                proxy: {
+                                    type: 'memory',
+                                    reader: {
+                                        type: 'json',
+                                        root: 'data'
+                                    }
+                                }
+                            });
+
+                            var window = Ext.create('Ext.window.Window', {
+                                modal: true,
+                                items: [
+                                    {
+                                        xtype: 'combobox',
+                                        store: store,
+                                        fieldLabel: 'Level',
+                                        queryMode: 'local',
+                                        displayField: 'text',
+                                        valueField: 'order'
+                                    },
+                                    {
+                                        xtype: 'combobox',
+                                        store: store,
+                                        fieldLabel: 'Target level',
+                                        queryMode: 'local',
+                                        displayField: 'text',
+                                        valueField: 'order'
+                                    },
+                                    {
+                                        xtype: 'combobox',
+                                        store: trainersStore,
+                                        fieldLabel: 'Trainer',
+                                        queryMode: 'local',
+                                        displayField: 'name',
+                                        valueField: 'order'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Add',
+                                        handler: function (btn, events) {
+                                            me.fireEvent('addInterview', me, {});
+                                            window.close();
+                                        }
+                                    }
+                                ]
+                            });
+                            window.show();
+                        }
+                    }
+                ]
+            }
+        ];
+
+        me.addEvents(
+            'addInterview'
+        );
+
+        me.callParent(arguments);
+    }
+});
