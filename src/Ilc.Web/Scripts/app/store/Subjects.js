42f19e383bead3bcb25385d26e343031486f2fbc
Maximilian <maximilian1991@gmail.com>
2013-07-25 15:58:59 +0300

    Added store files.

---
+++ src/Ilc.Web/Scripts/app/store/Subjects.js
@@ -0,0 +1,9 @@
+﻿Ext.define('Ilc.store.Subjects', {
+    extend: 'Ext.data.Store',
+    fields: ['id', 'name'],
+    
+    data: [
+        { id: 1, name: 'English' },
+        { id: 2, name: 'Romanian' }
+    ]
+});
---
+++ src/Ilc.Web/Scripts/app/store/TrainingSystems.js
@@ -0,0 +1,9 @@
+﻿Ext.define('Ilc.store.TrainingSystems', {
+    extend: 'Ext.data.Store',
+    fields: ['id', 'name'],
+    
+    data: [
+        { id: 1, name: 'European Union' },
+        { id: 2, name: 'Toeffel' }
+    ]
+});
