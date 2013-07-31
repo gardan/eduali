0f7df558aad6732cb2a068396a9faf77ed8ab978
Maximilian <maximilian1991@gmail.com>
2013-07-31 14:22:47 +0300

    When creating a new training students are shown based on the selected customer.

--- src/Ilc.Web/Scripts/app/view/trainings/Create.js
+++ src/Ilc.Web/Scripts/app/view/trainings/Create.js
@@ -48,13 +48,12 @@
         });
 
         customerComboBox.on('select', function (comboBox, records, eOpts) {
-            studentsStore.add([
-                { id: 1, name: 'Gheorghe Lazar', phone: '0723775755' },
-                { id: 2, name: 'Vasile Paiete', phone: '0723775755' },
-                { id: 3, name: 'Eric Lippert', phone: '0723775755' },
-                { id: 4, name: 'Razvan Negura', phone: '0723775755' },
-                { id: 5, name: 'Paltinisan Alexandru', phone: '0723775755' }
-            ]);
+            debugger;
+            studentsStore.load({
+                params: {
+                    customerId: records[0].data.id
+                }
+            });
         });
         
         me.items = [
