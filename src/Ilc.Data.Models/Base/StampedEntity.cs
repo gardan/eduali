249f746c06e4e538be35aa8b757a8d49fc85990c
Maximilian <maximilian1991@gmail.com>
2013-08-20 10:49:06 +0300

    Added locale file for nl, and a little ground work for the offer.

---
+++ src/Ilc.Data.Models/Base/StampedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ilc.Data.Models.Base
+{
+    public class StampedEntity : EntityBase
+    {
+        public DateTimeOffset CreateDate { get; set; }
+        public UserProfile Creator { get; set; }
+    }
+}
---
+++ src/Ilc.Data.Models/TrainingOffer.cs
@@ -0,0 +1,11 @@
+﻿using Ilc.Data.Models.Base;
+
+namespace Ilc.Data.Models
+{
+    public class TrainingOffer : StampedEntity
+    {
+        public decimal Price { get; set; }
+        public int NoLessons { get; set; }
+        public int LessonDuration { get; set; }
+    }
+}
