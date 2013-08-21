0a4fdd888493df0cda8641b2e524bfadd881ec7d
Maximilian <maximilian1991@gmail.com>
2013-08-22 01:20:04 +0300

    Added nl extjs locale

--- src/Ilc.Web/Services/TasksService.cs
+++ src/Ilc.Web/Services/TasksService.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Ilc.Data.Contracts;
 using Ilc.Web.Models;
 
 namespace Ilc.Web.Services
 {
     public class TasksService
     {
-        public FilteredDataModel<> Get(FilterParametersTasks request)
+        public IUow Uow { get; set; }
+
+        public FilteredDataModel<TaskModel> Get(FilterParametersTasks request)
         {
+            var trainings = Uow.Trainings.GetAll();
+
+            return null;
 
         }
+    }
+
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Action { get; set; }
     }
 
     public class FilterParametersTasks : FilterParametersBase
