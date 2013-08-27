fa27a4e5415aa64effd0970ddf4661e4e3913d6d
Maximilian <maximilian1991@gmail.com>
2013-08-27 12:40:19 +0300

    PdfView can now have an arbitrary name, we just need to pass another string when returing view from the controller.

---
+++ src/Ilc.Web/Controllers/PdfController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using Ilc.Data.Contracts;
+using Ilc.Web.Models;
+using RazorPDF;
+
+namespace Ilc.Web.Controllers
+{
+    public class PdfController : Controller
+    {
+        public IUow Uow { get; set; }
+
+        public ViewResult Offer(int id)
+        {
+            var offer = new LightOfferModel() { StartDate = DateTimeOffset.UtcNow };
+            var offer2 = Uow.Offers.GetById(id);
+
+            offer.NoLessons = offer2.NoLessons;
+            offer.LessonDuration = offer2.LessonDuration;
+            offer.PossibleCosts = offer2.Price.ToString();
+
+            return new PdfResult(offer, "Offer");
+        }
+    }
+}
