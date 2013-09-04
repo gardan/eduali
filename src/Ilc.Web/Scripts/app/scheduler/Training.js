100df6ad7f82d0e99b2816715db2ed1631ae5867
Maximilian <maximilian1991@gmail.com>
2013-09-04 15:17:36 +0300

    Fixed bug with scheduler when showing it a second time.

--- src/Ilc.Web/Scripts/app/scheduler/Training.js
+++ src/Ilc.Web/Scripts/app/scheduler/Training.js
@@ -1,10 +1,10 @@
 ﻿Ext.define('Ilc.scheduler.Training', {
     extend: 'Sch.panel.SchedulerGrid',
 
-    // allowOverlap: false,
-    // constrainDragToResource: true,
-    // snapToIncrement: true,
-    // eventResizeHandles: 'end',
+    allowOverlap: false,
+    constrainDragToResource: true,
+    snapToIncrement: true,
+    eventResizeHandles: 'end',
 
     // orientation: 'vertical',
 
@@ -15,59 +15,65 @@
     viewPreset: 'hourAndDay',
 
     // Setup your static columns
-    columns: [
-        { header: Ilc.resources.Manager.getResourceString('common.lessons'), width: 130, dataIndex: 'Name', visible: false }
-    ],
+    
 
     constructor: function () {
         var me = this;
 
+        // Fix for this: https://www.assembla.com/spaces/bryntum/support/tickets/13#/activity/ticket:
+
+        Ext.apply(this, {
+            columns: [
+                { header: Ilc.resources.Manager.getResourceString('common.lessons'), width: 130, dataIndex: 'Name', visible: false }
+            ]
+        });
+
         // zooming
-        // var slider = Ext.create('Ext.slider.SingleSlider', {
-        //     style: 'margin-left:10px',
-        //     width: 100,
-        //     value: 0,
-        //     increment: 1,
-        //     minValue: 0,
-        //     maxValue: 10,
-        //     listeners: {
-        //         change: function(p, v) {
-        //             me.zoomToLevel(parseInt(v), true);
-        //         }
-        //     }
-        // });
-        // 
-        // me.bbar = [
-        //     slider
-        // ];
-        // 
-        // me.listeners = {
-        //     eventcontextmenu: function(s, rec, e) {
-        //         e.stopEvent();
-        // 
-        //         if (!s.ctx) {
-        //             s.ctx = Ext.create('Ext.menu.Menu', {
-        //                 items: [{
-        //                     text: 'Delete event',
-        //                     iconCls: 'icon-delete',
-        //                     handler: function() {
-        //                         s.eventStore.remove(rec);
-        //                     }
-        //                 }]
-        //             });
-        //         }
-        //         s.ctx.rec = rec;
-        //         s.ctx.showAt(e.getXY());
-        //     },
-        //     afterlayout: function () {
-        //         slider.setMinValue(10);
-        //         slider.setMaxValue(14);
-        // 
-        //         slider.setValue(me.getCurrentZoomLevelIndex());
-        //     },
-        // 
-        //     single: true
-        // };
+         var slider = Ext.create('Ext.slider.SingleSlider', {
+             style: 'margin-left:10px',
+             width: 100,
+             value: 0,
+             increment: 1,
+             minValue: 0,
+             maxValue: 10,
+             listeners: {
+                 change: function(p, v) {
+                     me.zoomToLevel(parseInt(v), true);
+                 }
+             }
+         });
+         
+         me.bbar = [
+             slider
+         ];
+         
+         me.listeners = {
+             eventcontextmenu: function(s, rec, e) {
+                 e.stopEvent();
+         
+                 if (!s.ctx) {
+                     s.ctx = Ext.create('Ext.menu.Menu', {
+                         items: [{
+                             text: 'Delete event',
+                             iconCls: 'icon-delete',
+                             handler: function() {
+                                 s.eventStore.remove(rec);
+                             }
+                         }]
+                     });
+                 }
+                 s.ctx.rec = rec;
+                 s.ctx.showAt(e.getXY());
+             },
+             afterlayout: function () {
+                 slider.setMinValue(10);
+                 slider.setMaxValue(14);
+         
+                 slider.setValue(me.getCurrentZoomLevelIndex());
+             },
+         
+             single: true
+         };
 
         me.callParent(arguments);
     }
