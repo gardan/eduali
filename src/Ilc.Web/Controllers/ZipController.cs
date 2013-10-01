9f2814e7447fc8f16929481b09a0f50f75ea2c13
Maximilian <maximilian1991@gmail.com>
2013-10-01 17:05:28 +0300

    Made api for the zip file work. #26

--- src/Ilc.Web/Controllers/ZipController.cs
+++ src/Ilc.Web/Controllers/ZipController.cs
@@ -7,36 +7,63 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Ilc.Data.Contracts;
 
 namespace Ilc.Web.Controllers
 {
     public class ZipController : Controller
     {
 
+        public IUow Uow { get; set; }
+
         public FileResult Offers(OffersParameters request)
         {
-            var offer = new WebClient().DownloadData("http://localhost:54877/pdf/offer/1");
+            var client = new WebClient();
+            var training = Uow.Trainings.GetById(request.TrainingId);
+            var offerEntities = Uow.Offers.GetAll().Where(o => o.TrainingId == request.TrainingId).ToList();
+            var offers = new List<byte[]>();
 
-            ZipArchive archive;
+            foreach (var trainingOffer in offerEntities)
+            {
+                var host = "";
+                var port = 0;
+                var scheme = "";
+                if (Request.Url != null)
+                {
+                    host = Request.Url.Host;
+                    port = Request.Url.Port;
+                    scheme = Request.Url.Scheme;
+                }
 
+                var url = scheme + "://" + host + ":" + port + "/pdf/offer/" + trainingOffer.Id;
+
+                offers.Add(new WebClient().DownloadData(url));
+            }
+            
             using (var memStream = new MemoryStream())
             {
-                using (archive = new ZipArchive(memStream, ZipArchiveMode.Update,false))
+                using (var archive = new ZipArchive(memStream, ZipArchiveMode.Update,false))
                 {
-                    var entry = archive.CreateEntry("Offer1.pdf");
-
-                    using (var fileStream = new MemoryStream(offer))
+                    var i = 0;
+                    foreach (var offerFile in offers)
                     {
-                        using (var zipStream = entry.Open())
+                        var innerOffer = offerEntities[i];
+                        var name = "Offer-" + innerOffer.Id + "-" + innerOffer.Training.Customer.Name + ".pdf";
+                        var entry = archive.CreateEntry(name);
+
+                        using (var fileStream = new MemoryStream(offerFile))
                         {
-                            fileStream.CopyTo(zipStream);
+                            using (var zipStream = entry.Open())
+                            {
+                                fileStream.CopyTo(zipStream);
+                            }
                         }
+                        i++;
                     }
-                    
                 }
                 return new FileContentResult(memStream.ToArray(), "application/zip")
                     {
-                        FileDownloadName = "Zip1.zip"
+                        FileDownloadName = training.Customer.Name + "-" + training.Subject.Name + ".zip"
                     };
             }
 
