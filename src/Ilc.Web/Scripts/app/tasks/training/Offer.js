9498a3c4398649c4d7941c8c31b23181ac9f47f2
Maximilian <maximilian1991@gmail.com>
2013-10-07 10:19:03 +0300

    Added offers table to offer window.

--- src/Ilc.Web/Scripts/app/tasks/training/Offer.js
+++ src/Ilc.Web/Scripts/app/tasks/training/Offer.js
@@ -3,6 +3,13 @@
     
     xtype: 'offerwindow',
 
+    width: 500,
+
+    layout: 'anchor',
+    defaults: {
+        anchor: '100%'
+    },
+
     title: Ilc.resources.Manager.getResourceString('tasks.title.offer'),
 
     constructor: function (args) {
@@ -11,69 +18,81 @@
         var training = args.entity;
         var tasksStore = args.tasksStore;
 
-        // combobox - customers
-        
-        // combobox - trainings subject
-        // combobox - students ( multiple select )
-        
-        // textbox - duration
-        
-        // combobox - location, predefined cities
+        var offersStore = Ext.create('Ilc.store.Offers');
 
-        me.dockedItems = [
-            {
-                xtype: 'toolbar',
-                dock: 'bottom',
-                items: [
-                    {
-                        xtype: 'button',
-                        text: Ilc.resources.Manager.getResourceString('common.accepted'),
-                        handler: function () {
-                            var model = {
-                                taskEntityId: training.get('id'),
-                                action: 'Accepted' // This is a special string, that is defined in TrainingStatus.cs
-                            };
+        var offersGrid = Ext.create('Ilc.grid.Offers', {
+            store: offersStore
+        });
 
-                            me.fireEvent('addOffer', me, model, {
-                                tasksStore: tasksStore
-                            });
-                        }
-                    },
-                    {
-                        xtype: 'button',
-                        text: Ilc.resources.Manager.getResourceString('common.rejected'),
-                        handler: function () {
-                            var model = {
-                                taskEntityId: training.get('id'),
-                                action: 'Rejected' // This is a special string, that is defined in TrainingStatus.cs
-                            };
-                            me.fireEvent('addOffer', me, model, {
-                                tasksStore: tasksStore
-                            });
-                        }
-                    },
-                    '->',
-                    {
-                        xtype: 'button',
-                        text: Ilc.resources.Manager.getResourceString('common.close'),
-                        handler: function () {
-                            me.close();
-                        }
-                    }
-                ]
+        var acceptedOfferBtn = Ext.create('Ext.button.Button', {
+            text: Ilc.resources.Manager.getResourceString('common.accepted'),
+            handler: function () {
+                var model = {
+                    taskEntityId: training.get('id'),
+                    action: 'Accepted' // This is a special string, that is defined in TrainingStatus.cs
+                };
+
+                me.fireEvent('addOffer', me, model, {
+                    tasksStore: tasksStore
+                });
             }
+        });
+
+
+        offersGrid.on('newofferclick', function () {
+            // show the CreateRfi window
+            var createOfferWindow = Ext.create('Ilc.tasks.training.window.CreateRfi', {
+                closeAction: 'destroy',
+                entity: entity
+            });
+
+            createOfferWindow.on('addrfi', function (sender, model) {
+                // TODO: Need to modify the workflow to accept offers at this step aswell.
+            });
+
+            createOfferWindow.show();
+        });
+        
+        me.items = [
+            offersGrid
         ];
 
-        me.items = [
+        me.buttonAlign = 'left';
+
+        me.buttons = [
+            acceptedOfferBtn,
             {
-                xtype: 'label',
-                text: Ilc.resources.Manager.getResourceString('tasks.offer.mainLbl')
+                xtype: 'button',
+                text: Ilc.resources.Manager.getResourceString('common.rejected'),
+                handler: function () {
+                    var model = {
+                        taskEntityId: training.get('id'),
+                        action: 'Rejected' // This is a special string, that is defined in TrainingStatus.cs
+                    };
+                    me.fireEvent('addOffer', me, model, {
+                        tasksStore: tasksStore
+                    });
+                }
+            },
+            '->',
+            {
+                xtype: 'button',
+                text: Ilc.resources.Manager.getResourceString('common.close'),
+                handler: function () {
+                    me.close();
+                }
             }
         ];
 
         me.addEvents(
             'addOffer'
         );
+
+        offersStore.load({
+            params: {
+                trainingId: training.get('id')
+            }
+        });
 
         me.callParent(arguments);
     }
