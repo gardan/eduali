c75d5e779cf923a5000a36647ce860cad3eabb65
Maximilian <maximilian1991@gmail.com>
2013-11-11 16:21:28 +0200

    In statistics now only showing integers, no decimals.

--- src/Ilc.Web/Scripts/app/store/statistics/Customers.js
+++ src/Ilc.Web/Scripts/app/store/statistics/Customers.js
@@ -1,9 +1,21 @@
 ﻿Ext.define('Ilc.store.statistics.Customers', {
     extend: 'Ext.data.Store',
-    
-    fields: ['id', 'name', 'trainingsNo'],
-    
-    autoLoad: true,
+
+    fields: [
+        {
+            name: 'id',
+            type: 'int'
+        },
+        {
+            name: 'name',
+            type: 'string'
+        },
+        {
+            name: 'trainingsNo',
+            type: 'int'
+        }
+    ],
+
     proxy: {
         type: 'rest',
         url: 'api/statistics/customerTrainings',
--- src/Ilc.Web/Scripts/app/view/statistics/View.js
+++ src/Ilc.Web/Scripts/app/view/statistics/View.js
@@ -7,54 +7,74 @@
     },
 
     constructor: function () {
+        var me = this;
+
         var store = Ext.create('Ilc.store.statistics.Customers');
 
-        var chart = Ext.create('Ext.chart.Chart', {
-            animate: true,
-            shadow: true,
-            flex: 1,
-            maxHeight: 400,
-            store: store,
-            axes: [
-                {
-                    title: 'Trainigs',
-                    type: 'Numeric',
-                    position: 'bottom',
-                    fields: ['trainingsNo'],
-                    minimum: 0
-                },
-                {
-                    title: 'Customers',
-                    type: 'Category',
-                    position: 'left',
-                    fields: ['name'],
+        store.on('load', function (statsStore, records) {
+            var max = 0;
+            Ext.Array.forEach(records, function(record) {
+                if (record.get('trainingsNo') > max) {
+                    max = record.get('trainingsNo');
                 }
-            ],
-            
-            series: [
-                {
-                    type: 'bar',
-                    height: 100,
-                    axis: 'bottom',
-                    highlight: true,
-                    xField: 'name',
-                    yField: 'trainingsNo',
-                    tips: {
-                        trackMouse: true,
-                        layout: 'fit',
-                        items: [{xtype: 'label'}],
-                        renderer: function (storeItem, item) {
-                            this.down('label').setText(storeItem.get('name') + ': ' + storeItem.get('trainingsNo') + ' Trainings');
+            });
+
+            if (max < 30) {
+                max = 30;
+            }
+
+            var chart = Ext.create('Ext.chart.Chart', {
+                animate: true,
+                shadow: true,
+                flex: 1,
+                maxHeight: 400,
+                store: store,
+                axes: [
+                    {
+                        title: 'Trainigs',
+                        type: 'Numeric',
+                        position: 'bottom',
+                        fields: ['trainingsNo'],
+                        minimum: 0,
+                        maximum: max
+                    },
+                    {
+                        title: 'Customers',
+                        type: 'Category',
+                        position: 'left',
+                        fields: ['name'],
+                    }
+                ],
+
+                series: [
+                    {
+                        type: 'bar',
+                        height: 100,
+                        axis: 'bottom',
+                        highlight: true,
+                        xField: 'name',
+                        yField: 'trainingsNo',
+                        tips: {
+                            trackMouse: true,
+                            layout: 'fit',
+                            items: [{ xtype: 'label' }],
+                            renderer: function (storeItem, item) {
+                                this.down('label').setText(storeItem.get('name') + ': ' + storeItem.get('trainingsNo') + ' Trainings');
+                            }
                         }
                     }
-                }
-            ]
+                ]
+            });
+
+            me.add(chart);
+
         });
 
+        // this.items = [
+        //     chart
+        // ];
 
-        this.items = [
-            chart
-        ];
+        store.load();
 
         this.callParent(arguments);
     }
