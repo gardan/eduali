6c03415fec0413b65651336f2b85aa1af1ece8a2
Maximilian <maximilian1991@gmail.com>
2013-11-25 09:50:58 +0200

    Small code indentation fixed.

--- src/Ilc.Web/Scripts/app/grid/Users.js
+++ src/Ilc.Web/Scripts/app/grid/Users.js
@@ -16,8 +16,7 @@
             filter: {
                 type: 'string'
             }
-        }
-        ,
+        },
         {
             text: Ilc.resources.Manager.getResourceString('common.name'),
             dataIndex: 'userInfo',
@@ -25,8 +24,7 @@
             renderer: function (value) {
                 return value.name;
             }
-        }
-        ,
+        },
         {
             text: Ilc.resources.Manager.getResourceString('common.email'),
             dataIndex: 'userInfo',
@@ -34,8 +32,7 @@
             renderer: function (value) {
                 return value.email;
             }
-        }
-        ,
+        },
         {
             text: Ilc.resources.Manager.getResourceString('common.phone'),
             dataIndex: 'userInfo',
