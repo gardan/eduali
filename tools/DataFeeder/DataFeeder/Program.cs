6fbce5d481b975e86a8d5c4e93f590a7b73442f5
Vlad Glitia <axel_the_master@yahoo.com>
2013-11-30 15:08:50 +0200

    DataFeeder import changes

--- tools/DataFeeder/DataFeeder/Program.cs
+++ tools/DataFeeder/DataFeeder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,21 +14,29 @@ namespace DataFeeder
         /// <summary>
         /// Csv file is readed via FileUpload method.
         /// </summary>
-        public static List<string[]> parseCSV(Stream stream)
+        public static void parseCSV(Stream stream)
         {
-            List<string[]> parsedData = new List<string[]>();
-
             try
             {
                 using (StreamReader readFile = new StreamReader(stream))
                 {
+                    string cnStringImportdata = @"Data Source=localhost\sqlexpress;Initial Catalog=Ilc;Integrated Security=True";
+                    SqlConnection cn = new SqlConnection(cnStringImportdata);
                     string line;
                     string[] row;
 
                     while ((line = readFile.ReadLine()) != null)
                     {
                         row = line.Split(',');
-                        parsedData.Add(row);
+                        //skip the first line from csv file
+                        if(row[1].Contains("bankAccount"))
+                            continue;
+                        SqlCommand insertCommand =
+                            new SqlCommand("INSERT INTO Customer(Name, BillingAddress, BankAccount)" +
+                                           "Values('" + row[0] + "','" + row[2] + "','" + row[1] + "')", cn);
+                        cn.Open();
+                        insertCommand.ExecuteNonQuery();
+                        cn.Close();
                     }
                 }
             }
@@ -35,37 +44,34 @@ namespace DataFeeder
             {
                 //Response.Write("CSV parser error.<br>" + e.Message);
             }
-
-            return parsedData;
         }
 
         static void Main(string[] args)
         {
             //path to file
-            List<string[]> csv = parseCSV(File.OpenRead(@"H:\Diverse\Work\GitHub\ILC\tools\DataFeeder\DataFeeder\customers.csv"));
+            parseCSV(File.OpenRead(@"H:\Diverse\Work\GitHub\ILC\tools\DataFeeder\DataFeeder\customers.csv"));
 
             string error;
-            for (int i = 1; i < csv.Count; i++)
-            {
-                try
-                {
-                    string cnStringImportdata = @"Data Source=localhost\sqlexpress;Initial Catalog=Ilc;Integrated Security=True";
-
-                    using (SqlConnection cn = new SqlConnection(cnStringImportdata))
-                    {
-                        SqlCommand insertCommand =
-                            new SqlCommand("INSERT INTO Customer(Name, BillingAddress, BankAccount)" +
-                                           "Values('" + csv[i][0] + "','" + csv[i][2] + "','" + csv[i][1] + "')", cn);
-                        cn.Open();
-                        insertCommand.ExecuteNonQuery();
-                        cn.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                     Console.WriteLine(ex.Message);
-                }
-            }
+            string cnStringImportdata = @"Data Source=localhost\sqlexpress;Initial Catalog=Ilc;Integrated Security=True";
+            //using (SqlConnection cn = new SqlConnection(cnStringImportdata))
+//            for (int i = 1; i < csv.Count; i++)
+//            {
+//                try
+//                {
+//                    {
+//                        SqlCommand insertCommand =
+//                            new SqlCommand("INSERT INTO Customer(Name, BillingAddress, BankAccount)" +
+//                                           "Values('" + csv[i][0] + "','" + csv[i][2] + "','" + csv[i][1] + "')", cn);
+//                        cn.Open();
+//                        insertCommand.ExecuteNonQuery();
+//                        cn.Close();
+//                    }
+//                }
+//                catch (Exception ex)
+//                {
+//                     Console.WriteLine(ex.Message);
+//                }
+//            }
         }
     }
 }
