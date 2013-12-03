78fbc4837cac2b107f079b81a8827adff298523b
Maximilian <maximilian1991@gmail.com>
2013-12-03 17:49:10 +0200

    Added a spendings / price chart.

---
+++ src/Ilc.Web/Scripts/app/chart/Spendings.js
@@ -0,0 +1,32 @@
+﻿Ext.define('Ilc.chart.Spendings', {
+    extend: 'Ext.chart.Chart',
+    xtype: 'spendingschart',
+    anchor: '100%',
+    height: 500,
+    legend: {
+        position: 'right'
+    },
+    axes: [
+        {
+            position: 'bottom',
+            type: 'Numeric',
+            fields: ['price', 'spendings'],
+            title: 'Money'
+        },
+        {
+            position: 'left',
+            type: 'Category',
+            fields: ['customerName'],
+            title: 'Customers'
+        }
+    ],
+
+    series: [
+        {
+            type: 'bar',
+            axis: 'bottom',
+            xField: 'customerName',
+            yField: ['price', 'spendings']
+        }
+    ],
+});
---
+++ src/Ilc.Web/Scripts/app/store/statistics/Spendings.js
@@ -0,0 +1,133 @@
+﻿Ext.define('Ilc.store.statistics.Spendings', {
+    extend: 'Ext.data.Store',
+
+    fields: ['customerName', 'price', 'spendings'],
+
+    data: [
+        {
+            "customerName": "Aliquet Corp.",
+            "price": 880,
+            "spendings": 1023
+        },
+	    {
+	        "customerName": "Mi Eleifend Foundation",
+	        "price": 562,
+	        "spendings": 1458
+	    },
+	    {
+	        "customerName": "Blandit Viverra Donec Inc.",
+	        "price": 950,
+	        "spendings": 1251
+	    },
+	    {
+	        "customerName": "Ullamcorper Magna Sed Consulting",
+	        "price": 211,
+	        "spendings": 698
+	    },
+	    {
+	        "customerName": "Scelerisque Lorem LLC",
+	        "price": 978,
+	        "spendings": 160
+	    },
+	    {
+	        "customerName": "Venenatis Lacus Foundation",
+	        "price": 774,
+	        "spendings": 28
+	    },
+	    {
+	        "customerName": "Imperdiet Erat Consulting",
+	        "price": 646,
+	        "spendings": 1007
+	    },
+	    {
+	        "customerName": "Ut Molestie In Ltd",
+	        "price": 427,
+	        "spendings": 1081
+	    },
+	    {
+	        "customerName": "Donec Feugiat LLC",
+	        "price": 733,
+	        "spendings": 103
+	    },
+	    {
+	        "customerName": "Ipsum Corporation",
+	        "price": 263,
+	        "spendings": 896
+	    },
+	    {
+	        "customerName": "Semper Tellus Corp.",
+	        "price": 106,
+	        "spendings": 1106
+	    },
+	    {
+	        "customerName": "Convallis LLC",
+	        "price": 894,
+	        "spendings": 553
+	    },
+	    {
+	        "customerName": "Nulla Integer Corporation",
+	        "price": 463,
+	        "spendings": 1469
+	    },
+	    {
+	        "customerName": "Semper Cursus Integer Corporation",
+	        "price": 403,
+	        "spendings": 662
+	    },
+	    {
+	        "customerName": "Mi Lacinia Mattis Limited",
+	        "price": 360,
+	        "spendings": 756
+	    },
+	    {
+	        "customerName": "Rutrum Eu Consulting",
+	        "price": 838,
+	        "spendings": 1031
+	    },
+	    {
+	        "customerName": "Aliquam Enim LLP",
+	        "price": 679,
+	        "spendings": 717
+	    },
+	    {
+	        "customerName": "Pellentesque Corp.",
+	        "price": 620,
+	        "spendings": 1052
+	    },
+	    {
+	        "customerName": "Dolor Sit Corp.",
+	        "price": 675,
+	        "spendings": 587
+	    },
+	    {
+	        "customerName": "Suspendisse Ltd",
+	        "price": 315,
+	        "spendings": 1229
+	    },
+	    {
+	        "customerName": "Suspendisse Aliquet Sem PC",
+	        "price": 528,
+	        "spendings": 653
+	    },
+	    {
+	        "customerName": "A Consulting",
+	        "price": 191,
+	        "spendings": 1395
+	    },
+	    {
+	        "customerName": "Vel Lectus LLC",
+	        "price": 168,
+	        "spendings": 478
+	    },
+	    {
+	        "customerName": "Aptent Taciti Sociosqu Corporation",
+	        "price": 977,
+	        "spendings": 1258
+	    },
+	    {
+	        "customerName": "Curabitur Ut Odio Associates",
+	        "price": 922,
+	        "spendings": 118
+	    }
+    ]
+});
