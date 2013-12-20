6880081091b865e77022a0326f1ac64e45aa29ab
Maximilian <maximilian1991@gmail.com>
2013-12-20 13:31:43 +0200

    Added small sample of availability management UI goodiness.

---
+++ src/Ilc.Web/Scripts/app/model/Availability.js
@@ -0,0 +1,16 @@
+﻿Ext.define('Ilc.model.Availability', {
+    extend: 'Sch.model.Event',
+
+    fields: [
+        { name: 'id', type: 'int' },
+        { name: 'resourceId', type: 'int' },
+        { name: 'startDate', type: 'date', dateFormat: 'c' },
+        { name: 'endDate', type: 'date', dateFormat: 'c' }
+    ],
+    
+    idProperty: 'id',
+    // nameField: 'name',
+    startDateField: 'startDate',
+    endDateField: 'endDate',
+    resourceIdField: 'resourceId',
+});
--- src/Ilc.Web/Scripts/app/scheduler/Planning.js
+++ src/Ilc.Web/Scripts/app/scheduler/Planning.js
@@ -1,6 +1,10 @@
 ﻿Ext.define('Ilc.scheduler.Planning', {
     extend: 'Sch.panel.SchedulerGrid',
 
+    requires: [
+        'Ilc.model.Availability'
+    ],
+
     eventRenderer: function (eventRecord, resourceRecord, tmplData) {
         var style = 'background-color: ' + eventRecord.get('color');
         tmplData.style = style;
@@ -25,12 +29,22 @@
     ),
 
     enableDragCreation: false,
+    enabledHdMenu: false,
 
     plugins: [
                 new Sch.plugin.Pan({
                     enableVerticalPan: true
                 })
     ],
+
+    dndValidatorFn: function (dragRecords, targetResourceRecord, date, duration, e) {
+        var scheduler = this.up().up();
+        var availabilityStore = scheduler.resourceZones;
+        var result = availabilityStore.isResourceAvailable(targetResourceRecord, date, Ext.Date.add(date, Ext.Date.MILLI, duration));
+
+        console.log(result);
+        return result;
+    },
 
     initComponent: function() {
         var me = this;
@@ -85,6 +99,36 @@
                 }
             }
         ];
+
+        var availabilityStore = Ext.create('Sch.data.EventStore', {
+            model: 'Ilc.model.Availability',
+            
+            data: [
+                { id: 1, resourceId: 2, startDate: "2013-12-11T08:00:00", endDate: "2013-12-11T12:00:00" },
+                { id: 2, resourceId: 3, startDate: "2013-12-11T08:00:00", endDate: "2013-12-11T12:00:00" }
+                // { ResourceId: 2, StartDate: "2011-09-01T10:00", EndDate: "2011-09-01T16:00" }
+            ],
+            
+            isResourceAvailable: function (resource, start, end) {
+                var availability = this.getEventsForResource(resource);
+
+                if (!availability || availability.length === 0) return true;
+
+                for (var i = 0; i < availability.length; i++) {
+                    console.log(availability[i].getStartDate());
+                    console.log(availability[i].getEndDate());
+                    console.log(start);
+                    console.log(end);
+                    if (Sch.util.Date.timeSpanContains(availability[i].getStartDate(), availability[i].getEndDate(), start, end)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+        
+        me.resourceZones = availabilityStore;
 
         me.callParent(arguments);
     }
