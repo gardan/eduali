eda7918f833120b6de8225bea7379a079d284b4c
Maximilian <maximilian1991@gmail.com>
2014-01-22 14:31:47 +0200

    Added icons to main menu buttons.

--- src/Ilc.Web/Scripts/app/view/Viewport.js
+++ src/Ilc.Web/Scripts/app/view/Viewport.js
@@ -17,47 +17,55 @@
             itemId: 'main-nav-toolbar',
             defaults: {
                 scale: 'large',
-                padding: '0 20',
+                // padding: '0 10',
                 toggleGroup: 'main-nav',
                 allowDepress: false
             },
             items: [
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.tasks'),
+                    iconCls: 'icon-task-32',
                     itemId: 'menu.tasks'
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.trainings'),
-                    itemId: 'menu.trainings',
-                    pressed: true
+                    iconCls: 'icon-book-32',
+                    itemId: 'menu.trainings'
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.customers'),
+                    iconCls: 'icon-customer-32',
                     itemId: 'menu.customers',
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.students'),
+                    iconCls: 'icon-student-32',
                     itemId: 'menu.students',
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.statistics'),
+                    iconCls: 'icon-statistics-32',
                     itemId: 'menu.statistics',
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.trainers'),
+                    iconCls: 'icon-trainer-32',
                     itemId: 'menu.trainers',
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.settings'),
+                    iconCls: 'icon-settings-32',
                     itemId: 'menu.settings',
                     hidden: !Ilc.Configuration.claimExists('ui-settings-read')
                 },
                 {
                     text: Ilc.resources.Manager.getResourceString('mainMenu.planning'),
+                    iconCls: 'icon-planning-32',
                     itemId: 'menu.planning'
                 },
                 {
                     text: 'Availability',
+                    iconCls: 'icon-availability-32',
                     itemId: 'menu.availability',
                     hidden: !Ilc.Configuration.claimExists('tasks-trainer') && !Ilc.Configuration.claimExists('availability-read'),
                     listeners: {
