71c21d7a9450cb88247f86fd31e9e3e26586fb05
Maximilian <maximilian1991@gmail.com>
2014-02-06 12:24:00 +0200

    UserDetails are now properly populated.

--- src/Ilc.Web/Services/UsersService.cs
+++ src/Ilc.Web/Services/UsersService.cs
@@ -41,7 +41,7 @@ namespace Ilc.Web.Services
                 {
                     Username = request.Username,
                     Roles = roles,
-                    UserDetails = new UserDetails().InjectFrom(request.UserInfo) as UserDetails
+                    UserDetails = new UserDetails().InjectFrom<UserInfoModelToUserDetails>(request.UserInfo) as UserDetails
                 };
 
             Users.Create(userProfile, request.Password);
