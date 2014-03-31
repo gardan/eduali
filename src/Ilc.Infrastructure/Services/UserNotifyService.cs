24de53d96cc317c1bc39dc2cd2bf0e64d68cf2c5
Maximilian <maximilian1991@gmail.com>
2014-03-31 18:01:40 +0300

    The application can now send an email, and does so, only when in debug mode. Also i've added templating capabilities with the help ss.razor. Some notes on this: i had had to manually add the v3 system.web.razor.dll to the references because adding it via nuget fucked things up.

--- src/Ilc.Infrastructure/Services/UserNotifyService.cs
+++ src/Ilc.Infrastructure/Services/UserNotifyService.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web.Configuration;
+using System.Web.Helpers;
+using Ilc.Core.Contracts;
 using Ilc.Infrastructure.Contracts;
 
 namespace Ilc.Infrastructure.Services
 {
     public class UserNotifyService : IUserNotifyService
     {
+        public IUsersService Users { get; set; }
+
         public void Notify(int userId, string body)
         {
+            var email = Users.GetById(userId);
+            SendMail(email.Email, "", body);
+        }
+
+        private void SendMail(string to, string subject, string body)
+        {
+            WebMail.SmtpServer = WebConfigurationManager.AppSettings["SMTPHost"];
+            WebMail.SmtpPort = Convert.ToInt32(WebConfigurationManager.AppSettings["SMTPPort"]);
+            WebMail.UserName = WebConfigurationManager.AppSettings["SMTPUsername"];
+            WebMail.Password = WebConfigurationManager.AppSettings["SMTPPassword"];
+            WebMail.EnableSsl = Convert.ToBoolean(WebConfigurationManager.AppSettings["SMTPEnableSsl"]);
+
+            WebMail.From = "admin@eduali.com";
             
+            WebMail.Send(to, subject, body);
         }
     }
 }
