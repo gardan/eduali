d345e3e3ae6942a9e818ed12f7a237a7aef646bb
Maximilian <maximilian1991@gmail.com>
2014-04-10 16:19:24 +0300

    Removed text from menu buttons.

--- src/Ilc.Web/Scripts/app/view/Viewport.js
+++ src/Ilc.Web/Scripts/app/view/Viewport.js
@@ -26,71 +26,126 @@
     _initMenuButtons: function() {
         return [
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.tasks'),
                 iconCls: 'icon-task-32',
                 itemId: 'menu.tasks',
-                hidden: !Ilc.Configuration.loggedIn()
+                hidden: !Ilc.Configuration.loggedIn(),
+                tooltip:  {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.tasks'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                } // ,
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.trainings'),
                 iconCls: 'icon-book-32',
                 itemId: 'menu.trainings?open=true',
-                hidden: !Ilc.Configuration.claimExists('tasks-student')
+                hidden: !Ilc.Configuration.claimExists('tasks-student'),
+                tooltip: {
+                    text: 'Open trainings',
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.trainings'),
                 iconCls: 'icon-book-32',
                 itemId: 'menu.trainings',
-                hidden: !Ilc.Configuration.claimExists('training-read-all')
+                hidden: !Ilc.Configuration.claimExists('training-read-all'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.trainings'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.companies'),
                 iconCls: 'icon-company-32',
                 itemId: 'menu.companies',
-                hidden: !Ilc.Configuration.claimExists('company-read-all')
+                hidden: !Ilc.Configuration.claimExists('company-read-all'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.companies'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.customers'),
                 iconCls: 'icon-customer-32',
                 itemId: 'menu.customers',
-                hidden: !Ilc.Configuration.claimExists('customer-read-all')
+                hidden: !Ilc.Configuration.claimExists('customer-read-all'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.customers'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.students'),
                 iconCls: 'icon-student-32',
                 itemId: 'menu.students',
-                hidden: !Ilc.Configuration.claimExists('student-read-all')
+                hidden: !Ilc.Configuration.claimExists('student-read-all'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.students'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.statistics'),
                 iconCls: 'icon-statistics-32',
                 itemId: 'menu.statistics',
-                hidden: !Ilc.Configuration.claimExists('statistics-read')
+                hidden: !Ilc.Configuration.claimExists('statistics-read'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.statistics'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.trainers'),
                 iconCls: 'icon-trainer-32',
                 itemId: 'menu.trainers',
-                hidden: !Ilc.Configuration.claimExists('trainer-read-all')
+                hidden: !Ilc.Configuration.claimExists('trainer-read-all'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.trainers'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.settings'),
                 iconCls: 'icon-settings-32',
                 itemId: 'menu.settings',
-                hidden: !Ilc.Configuration.claimExists('ui-settings-read')
+                hidden: !Ilc.Configuration.claimExists('ui-settings-read'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.settings'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: Ilc.resources.Manager.getResourceString('mainMenu.planning'),
                 iconCls: 'icon-planning-32',
                 itemId: 'menu.planning',
-                hidden: !Ilc.Configuration.claimExists('planning-read')
+                hidden: !Ilc.Configuration.claimExists('planning-read'),
+                tooltip: {
+                    text: Ilc.resources.Manager.getResourceString('mainMenu.planning'),
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                }
             },
             {
-                text: 'Availability',
                 iconCls: 'icon-availability-32',
                 itemId: 'menu.availability',
                 border: false,
                 hidden: !Ilc.Configuration.claimExists('tasks-trainer') && !Ilc.Configuration.claimExists('availability-read'),
+                tooltip: {
+                    text: 'Availability',
+                    width: 100,
+                    showDelay: 0,
+                    hideDelay: 50
+                },
                 listeners: {
                     click: function(panel) {
                         var viewport = panel.up('viewport');
