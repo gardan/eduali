7f729ffd19a31581fc066106fcb37872fff9cdd1
Maximilian <maximilian1991@gmail.com>
2014-04-10 15:37:39 +0300

    User can now login from the presentation layer. closes #101

---
+++ src/Eduali.WebPresentation/Eduali.WebPresentation/Scripts/login.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+
+    $('#loginButton').on('click', function() {
+        var inputs = $('.login-input');
+
+        var data = {
+            email: inputs[0].value,
+            password: inputs[1].value
+        };
+
+        $.ajax({
+            url: 'app/api/auth',
+            type: 'POST',
+            contentType: 'application/json',
+            converters: {
+                'text json': function (rawData) {
+                    if (rawData == '') return {};
+
+                    return $.parseJSON(rawData);
+                }
+            },
+            data: JSON.stringify(data)
+        })
+        .done(function () {
+            window.location = 'app/';
+        })
+        .fail(function () {
+            console.log(arguments);
+        })
+        .always(function () {
+            $.loader("close");
+        });
+
+    });
+   
+});
