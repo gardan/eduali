f46f16e5da3acc32ec861bed25313c6d027f98da
Maximilian <maximilian1991@gmail.com>
2014-06-02 23:16:06 +0300

    Added editor window to file templates. #122

--- src/Ilc.Web/Scripts/app/view/fileTemplates/List.js
+++ src/Ilc.Web/Scripts/app/view/fileTemplates/List.js
@@ -1,13 +1,53 @@
 ﻿Ext.define('Ilc.view.fileTemplates.List', {
     extend: 'Ext.container.Container',
     
+    onItemDoubleClick: function (grid, record) {
+        var editor = Ext.create('Ilc.form.Editor', {
+            value: record.get('content')
+        });
+
+        var window = Ext.create("Ext.window.Window", {
+            width: 800,
+            height: 400,
+            items: [
+                {
+                    xtype: 'textfield',
+                    name: 'name',
+                    value: record.get('name')
+                },
+                editor     
+            ],
+            buttons: [
+                {
+                    text: 'Save',
+                    listeners: {
+                        click: function() {
+                            record.set('content', editor.getValue());
+
+                            record.save();
+                            record.commit();
+                            console.log(record);
+                        },
+                        scope: this
+                    }
+                }
+            ]
+        });
+
+        window.show();
+    },
+
     initComponent: function() {
 
         var fileTemplatesStore = Ext.create('Ilc.store.FileTemplates');
         fileTemplatesStore.load();
 
         var fileTemplatesGrid = Ext.create('Ilc.grid.FileTemplates', {
-            store: fileTemplatesStore
+            store: fileTemplatesStore,
+            listeners: {
+                itemdblclick: this.onItemDoubleClick,
+                scope: this
+            }
         });
 
         this.items = [
