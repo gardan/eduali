a471e555ef0afd67fecd3f781113be593b2c8d97
Maximilian <maximilian1991@gmail.com>
2015-05-31 19:26:36 +0300

    Added delete for shift templates. #213

--- src/Ilc.Web/Scripts/app/view/templates/List.js
+++ src/Ilc.Web/Scripts/app/view/templates/List.js
@@ -13,6 +13,29 @@
                     dataIndex: 'name',
                     text: Ilc.resources.Manager.getResourceString('common.name'),
                     flex: 1
+                },
+                {
+                    xtype: 'actioncolumn',
+                    items: [
+                        {
+                            icon: 'images/web/remove.png',
+                            tooltip: Ilc.resources.Manager.getResourceString('common.delete'),
+                            handler: function (grid, rowIndex, colIndex, item, e, record) {
+                                record.destroy({
+                                    success: function () {
+                                        console.log(arguments);
+                                    },
+                                    failure: function (model, response) {
+                                        Ext.MessageBox.show({
+                                            title: 'Error',
+                                            msg: response.error.statusText,
+                                            buttons: Ext.MessageBox.OK,
+                                        });
+                                    }
+                                });
+                            }
+                        }
+                    ]
                 }
             ],
             store: templatesStore,
